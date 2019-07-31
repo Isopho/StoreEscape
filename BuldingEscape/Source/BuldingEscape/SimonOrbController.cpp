9156628eea3cdcb4376c5160ff81ca9d48e0e6d0
Isopho <jan-uriel.lorbeer@web.de>
2019-07-31 15:07:44 +0200

    Added Basic Orbs with lighting and controller sricpt for the SimonSays Orb Game

---
+++ BuldingEscape/Source/BuldingEscape/SimonOrbController.cpp
@@ -0,0 +1,47 @@
+// Copyright Jan-Uriel Lorbeer 2019.
+
+
+#include "SimonOrbController.h"
+
+// Sets default values for this component's properties
+USimonOrbController::USimonOrbController()
+{
+	// Set this component to be initialized when the game starts, and to be ticked every frame.  You can turn these features
+	// off to improve performance if you don't need them.
+	PrimaryComponentTick.bCanEverTick = true;
+
+	// ...
+}
+
+
+// Called when the game starts
+void USimonOrbController::BeginPlay()
+{
+	Super::BeginPlay();
+
+	
+}
+
+
+// Called every frame
+void USimonOrbController::TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction)
+{
+	Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
+
+	if (GetWorld()->GetTimeSeconds() - realtimeSeconds > 5.0f)
+	{
+		realtimeSeconds = GetWorld()->GetTimeSeconds();
+
+		UE_LOG(LogTemp, Warning, TEXT("Flaring Simon orb: %s"), *(GetOwner()->GetName()));
+		FlareSimonOrb();
+	}
+
+
+	// ...
+}
+
+void USimonOrbController::FlareSimonOrb() const
+{
+	FlareOrbRequest.Broadcast(1.0f);
+}
+
