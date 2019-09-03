e39a4c88b494dacab4ba115275c560b578460e32
Isopho <jan-uriel.lorbeer@web.de>
2019-09-03 08:58:27 +0200

    *fixed deployment bug

--- Escape/Source/Escape/ObjectivesManager.h
+++ Escape/Source/Escape/ObjectivesManager.h
@@ -4,7 +4,6 @@
 
 #include "CoreMinimal.h"
 #include "Components/ActorComponent.h"
-#include "Components/Widget.h"
 #include "GameFramework/PlayerController.h"
 #include "GameFramework/HUD.h"
 #include "Engine/World.h"
